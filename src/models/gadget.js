453b9c61bdd50af400177627f8f68ee6be2c7c2b
rajchhatwani <111250278+rajchhatwani@users.noreply.github.com>
2025-02-06 14:18:05 +0530

    resolving issues

--- src/models/gadget.js
+++ src/models/gadget.js
@@ -26,39 +26,6 @@ const Gadget = sequelize.define("Gadget", {
   },
 });
 
-Gadget.sync().then(() => console.log("Gadget table is created")).catch((err)=> console.log("error while creating this tableName",err));
-
-
-module.exports = Gadget;
-
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db");
-
-const Gadget = sequelize.define("Gadget", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.ENUM(
-      "Available",
-      "Deployed",
-      "Destroyed",
-      "Decommissioned"
-    ),
-    defaultValue: "Available",
-  },
-  decommissionedAt: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-});
-
 Gadget.sync()
   .then(() => console.log("Gadget table is created"))
   .catch((err) => console.log("error while creating this tableName", err));
